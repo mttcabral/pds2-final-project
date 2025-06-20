c27930af3819bd4e7015c3f02efdb4d3cd00be8f
Pedro <pedroaugustosilva166@gmail.com>
2025-06-19 21:43:36 -0300

    implement struct Rectangle

--- src/table.cpp
+++ src/table.cpp
@@ -37,3 +37,34 @@ Color::Color(float r, float g, float b) : r(r), g(g), b(b) {};
 void Color::display(){
     cout<< "RGB: (" << this->r << "," << this->g << "," << this->b << ")" << endl;
 }
+
+Rectangle::Rectangle() : Rectangle(Point(0.0, 0.0), 0.0, 0.0) {};
+Rectangle::Rectangle(Point center, float length, float height) : center(center), length(length), height(height) {
+    this->topLeft = center + Point(((float) -length)/(2.0), ((float) -height)/(2.0));
+    this->bottomRight = center + Point(length/2, height/2);
+}
+
+Rectangle::Rectangle(Point topLeft, Point bottomRight) : topLeft(topLeft), bottomRight(bottomRight) {
+    this->center = (topLeft + bottomRight)/(2.0);
+    this->length = (bottomRight.x - topLeft.x);
+    this->height = (bottomRight.y - topLeft.y);
+}
+
+Rectangle& Rectangle::operator = (const Rectangle& otherRectangle) {
+    this->topLeft = otherRectangle.topLeft;
+    this->bottomRight = otherRectangle.bottomRight;
+    this->center = otherRectangle.center;
+    this->length = otherRectangle.length;
+    this->height = otherRectangle.height;
+    return *this;
+}
+
+void Rectangle::display(){
+    cout<<"Top Left: ";
+    this->topLeft.display();
+    cout<<"Center: ";
+    this->center.display();
+    cout<<"Bottom Right: ";
+    this->bottomRight.display();
+    cout<<endl;
+}
