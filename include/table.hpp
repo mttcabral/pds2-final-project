4fae2c596d4ecea6a78d4d352fd7bded6db87189
Pedro <pedroaugustosilva166@gmail.com>
2025-06-19 21:31:59 -0300

    Includes and guards in the table.hpp

---
+++ include/table.hpp
@@ -0,0 +1,12 @@
+#ifndef TABLE_H
+#define TABLE_H
+#include <vector>
+#include <stdbool.h>
+#include <string>
+#include <vector>
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+#endif
