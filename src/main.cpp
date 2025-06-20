d795b40cdcef8184257be02496ac59fa6132ec54
Callszone <cauanetoescola@gmail.com>
2025-06-20 11:26:18 -0300

    made things cleaner

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,8 @@
 #include "cooldown.hpp"
 #include <iostream>
 #include <string>
+#include "load.hpp"
 
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
 
 using namespace std;
 
@@ -40,6 +39,7 @@ int main(){
 
     if(!initialize_display_and_timer(display,SCREEN_W,SCREEN_H,timer,FPS)) return 1;
     if(!al_is_mouse_installed()) return 1;
+    if(!load_check()) return 1;
 
     // Register event sources for the event queue
     al_register_event_source(eventQueue, al_get_display_event_source(display));
@@ -66,26 +66,6 @@ int main(){
     //Pipe obstacle(Point(200,600),128,128);
     obstacle.loadSprite("assets/long.png");
 
-    // loading images and font
-    ALLEGRO_BITMAP* menu_background = al_load_bitmap("assets/menu_background.png");
-    ALLEGRO_BITMAP* play_button = al_load_bitmap("assets/menu_play_button.png");
-    ALLEGRO_BITMAP* quit_button = al_load_bitmap("assets/menu_quit_button.png");
-    ALLEGRO_BITMAP* hover_play = al_load_bitmap("assets/menu_hover_play_button.png");
-    ALLEGRO_BITMAP* hover_quit = al_load_bitmap("assets/menu__hover_quit_button.png");
-    ALLEGRO_FONT* menu_font = al_load_ttf_font("assets/PressStart2P-Regular.ttf", 72, 0);
-
-    if (!menu_background) std::cerr << "Erro: imagem menu_background não foi carregada!\n";
-    if (!play_button)     std::cerr << "Erro: imagem play_button não foi carregada!\n";
-    if(!hover_play) std::cerr << "Erro: imagem hover_play não foi carregada \n";
-    if (!quit_button)     std::cerr << "Erro: imagem quit_button não foi carregada!\n";
-    if(!hover_quit) std::cerr << "Erro: imagem hover_quit não foi carregada\n";
-    if(!menu_font) std::cerr << "Erro: fonte menu_font não foi carregada \n";
-
-
-    // seeing if everything is alright
-    if(!menu_background || !play_button || !quit_button || !menu_font){
-        return 1;
-    }
  
     //coordinates of play and quit
     int xplay = 350, yplay = 350;
