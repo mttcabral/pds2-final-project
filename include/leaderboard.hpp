03767f6940cc32129e42fafcceb12626b8818f7b
Pedro <pedroaugustosilva166@gmail.com>
2025-06-24 12:50:20 -0300

    fixed leaderboard

--- include/leaderboard.hpp
+++ include/leaderboard.hpp
@@ -1,16 +1,11 @@
 #ifndef LEADERBOARD_H
 #define LEADERBOARD_H
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_image.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_audio.h>
-#include <allegro5/allegro_acodec.h>
 #include <set>
 #include <vector>
 #include <string>
-#include <stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -28,22 +23,20 @@ class LeaderBoard {
 
         LeaderBoard(string, RectangleT);
 
-        void newProfile(Profile);
+        bool addNewProfile(Profile);
         void updateLeaderBoard();
 
-        void setTitleRowColor(Color);
+        //void setTitleRowColor(Color);
         void setFirstRowColor(Color);
         void setSecondRowColor(Color);
         void setThirdRowColor(Color);
         void setOthersRowsColor(Color);
 
-        void setTitleRowTextColor(Color);
+        //void setTitleRowTextColor(Color);
         void setFirstRowTextColor(Color);
         void setSecondRowTextColor(Color);
         void setThirdRowTextColor(Color);
         void setOthersRowsTextColor(Color);
-
-        bool close();
 
         void drawLeaderBoard(ALLEGRO_FONT*);
 
