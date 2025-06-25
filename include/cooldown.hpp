80cdefea313a531b54a7f2964f78d0b8ff4f23cb
Mateus Cabral <mateusmacabral@gmail.com>
2025-06-25 02:49:39 -0300

    chore: format source code in cooldown.hpp

--- include/cooldown.hpp
+++ include/cooldown.hpp
@@ -3,47 +3,47 @@
 
 // class for handling time events, such as obstacle spawning or action cooldowns
 
-//keep this synced with 'main' FPS
+// keep this synced with 'main' FPS
 const float FPS = 30;
 
-class Cooldown {
-    private:
-        //Frames left to finish cooldown
-        float timeLeft = 0;
-        //how much time it takes to finish cooldown each time
-        float rechargeTime;
-        //how fast it counts down, 1 is normal time and X is X times faster
-        float updateFrequency = 1;
-        bool frozen = false;
-    public:
-        //always input time IN SECONDS units
-        Cooldown(float wuTime);
-        Cooldown();
+class Cooldown
+{
+private:
+    // Frames left to finish cooldown
+    float timeLeft = 0;
+    // how much time it takes to finish cooldown each time
+    float rechargeTime;
+    // how fast it counts down, 1 is normal time and X is X times faster
+    float updateFrequency = 1;
+    bool frozen = false;
 
-        //count timeLeft down until it finishes
-        void updateCooldown();
+public:
+    // always input time IN SECONDS units
+    Cooldown(float wuTime);
+    Cooldown();
 
-        //resets timeLeft to rechargeTime
-        void restartCooldown();
-        //sets timeLeft to 0
-        void refreshCooldown();
+    // count timeLeft down until it finishes
+    void updateCooldown();
 
-        //outputs rather the cooldown is finished or not
-        bool isCooldownUp();
+    // resets timeLeft to rechargeTime
+    void restartCooldown();
+    // sets timeLeft to 0
+    void refreshCooldown();
 
-        //sets new wind up time, remember to input time IN SECONDS units
-        void setRechargeTime(float wuTime);
-        //new update frequency factor, 1 is normal and 2 is twice as fast, for example
-        void setUpdateFrequency(float f);
+    // outputs rather the cooldown is finished or not
+    bool isCooldownUp();
 
-        float getCurrentTimeLeft();
-        float getCurrentPorcentage();
-        float getRechargeTime();
+    // sets new wind up time, remember to input time IN SECONDS units
+    void setRechargeTime(float wuTime);
+    // new update frequency factor, 1 is normal and 2 is twice as fast, for example
+    void setUpdateFrequency(float f);
 
-        void freezeCooldown();
-        void unfreezeCooldown();
+    float getCurrentTimeLeft();
+    float getCurrentPorcentage();
+    float getRechargeTime();
+
+    void freezeCooldown();
+    void unfreezeCooldown();
 };
-
-
 
 #endif
